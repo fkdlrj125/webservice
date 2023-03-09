f25d2f5ac835fd680c2dc96b290fcd7969fd489d
이정현 <fkdlrj1250@gmail.com>
2023-03-09 22:42:42 +0900

    230309:update

--- file/workspace/p230309/src/method/reference/MethodReferenceQuiz.java
+++ file/workspace/p230309/src/method/reference/MethodReferenceQuiz.java
@@ -45,9 +45,18 @@ public class MethodReferenceQuiz {
 		Consumer<Person> lambdaConsumer = (Person p) -> System.out.println(p);
 		forEach(person, lambdaConsumer);
 		
-		// [메소드 출력]
+		// [메소드 참조]
+//		메소드 참조는 정의된 메소드의 body를 가져와서 사용하는 형태다 
+//		현재는 Consumer functionalInterface의 accept 메소드를 오버라이딩해서 
+//		그 메소드를 참조하는 형태인데 이 상태에서는 람다식을 사용하면 현재 참조할려는
+//		메소드의 타입을 알 수 없기 때문에 사용할 수 없다
 		System.out.println("\n<< Member forEach Method Reference >>");
-		Consumer<Person> referenceConsumer = lambdaConsumer::accept;
+		Consumer<Person> referenceConsumer = new Consumer<Person>() {		  
+			@Override														
+			public void accept(Person t) {									
+				System.out.println(t);
+			}
+		}::accept;	
 		forEach(person, referenceConsumer);
 		
 		// [문제 1-2] 이름만 출력 : getName()
@@ -56,9 +65,14 @@ public class MethodReferenceQuiz {
 		Consumer<Person> lambdaName = (Person p) -> System.out.println(p.getName());;
 		forEach(person, lambdaName);
 		
-		// [메소드 출력]
+		// [메소드 참조]
 		System.out.println("\n<< Member forEach Method Reference >>");
-		Consumer<Person> referenceName = lambdaName::accept;
+		Consumer<Person> referenceName = new Consumer<Person>() {
+			@Override
+			public void accept(Person t) {
+				System.out.println(t.getName());
+			}
+		}::accept;
 		forEach(person, referenceName);
 		
 		// [문제 2] ArrayList의 forEach문 사용
@@ -75,7 +89,7 @@ public class MethodReferenceQuiz {
 		// [람다]
 		System.out.println("\n<< ArrayList forEach lambda >> ");
 		((ArrayList<Person>)person).forEach(lambdaName);
-		Math.max(0, 0)
+		
 		// [메소드 참조]
 		System.out.println("\n<< ArrayList forEach Method Reference >> ");
 		((ArrayList<Person>)person).forEach(referenceName);
