1506a75560e9249a4d32c40a9edf8f3b7c027760
fkdlrj125 <39903663+fkdlrj125@users.noreply.github.com>
2023-03-24 18:22:43 +0900

    230324
    Thymeleaf(@{}), Lombok(log), thread

---
+++ file/workspace/spring-fourth/src/main/java/himedia/fourth/controller/StoreController.java
@@ -0,0 +1,66 @@
+package himedia.fourth.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import himedia.fourth.domain.Item;
+import himedia.fourth.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/store/items")
+public class StoreController {
+	private final ItemRepository repository;
+	
+	public StoreController(ItemRepository repository) {
+		this.repository= repository;
+	}
+	
+	@GetMapping
+	public String items(Model model) {
+		List<Item> list = repository.findAll();
+		model.addAttribute("itemlist", list);
+		return "store/items";
+	}
+	
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable long itemId,Model model) {
+		Item item = repository.findById(itemId).get();
+		model.addAttribute("item",item);
+		return "store/item";
+	}
+	
+	@GetMapping("/add")
+	public String getAddForm() {
+		return "store/addForm";
+	}
+	
+	@PostMapping("/add")
+	public String postAddForm(Item item,Model model) {
+		repository.save(item);
+		return "redirect:/store/items/"+item.getId();
+	}
+	
+	@GetMapping("/{itemId}/edit")
+	public String getEditForm(@PathVariable Long itemId, Model model) {
+		Item item = repository.findById(itemId).get();
+		model.addAttribute("item", item);
+		return "store/editForm";
+	}
+	
+	@PostMapping("/{itemId}/edit")
+	public String postEditForm(Item item,Model model) {
+		log.info("id >> "+item.getId());
+		repository.update(item.getId(), item);
+		model.addAttribute("item",item);
+		return "redirect:/store/items"+item.getId();
+	}
+	
+}
---
+++ file/workspace/spring-fourth/src/test/java/himedia/fourth/repository/JdbcTemplateItemRepositoryTest.java
@@ -0,0 +1,103 @@
+package himedia.fourth.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.transaction.annotation.Transactional;
+
+import himedia.fourth.domain.Item;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+class JdbcTemplateItemRepositoryTest {
+
+	@Autowired JdbcTemplateItemRepository repository;
+	
+	@Test
+	@Transactional
+	void save() {
+//		given
+		Item item = new Item("진라면",1000,100);
+		
+//		when
+		Item result = repository.save(item);
+		
+//		then
+		assertThat(result.getId()).isEqualTo(item.getId());
+		
+		log.info("item.getItemName() >> "+result.getItemName());
+		log.info("item.getItemName() >> {}",result.getItemName());
+	}
+	
+	@Test
+	void findId() {
+//		given
+		Item item = new Item("신라면",1200,200);
+		repository.save(item);
+		
+//		when
+		Item result = repository.findById(item.getId()).get();
+		
+//		then
+		assertThat(result.getId()).isEqualTo(item.getId());
+	}
+	
+	@Test
+	void findName() {
+//		given
+		Item item = new Item("삼양라면",1100,50);
+		repository.save(item);
+		
+//		when
+		Item result = repository.findByName(item.getItemName()).get();
+		
+//		then
+		assertThat(result.getItemName()).isEqualTo(item.getItemName());
+	}
+	
+	@Test
+	void findAll() {
+//		given
+		int before = repository.findAll().size();
+		Item item1 = new Item("불닭볶음면",1400,500);
+		Item item2 = new Item("짜파게티", 1400, 400);
+		repository.save(item1);
+		repository.save(item2);
+		
+//		when
+		List<Item> result = repository.findAll();
+		
+//		then
+		assertThat(result.size()).isEqualTo(before+2);
+	}
+	
+	@Test
+	void update() {
+//		given
+		List<Item> list = repository.findAll();
+		Long key = list.stream().findAny().get().getId();
+		Item update = new Item("진짬뽕",1800,350);
+		log.info("key >> {}",key);
+		
+//		when
+		Item result = repository.update(key, update);
+		log.info("result.getItemName() >> {}",result.getItemName());
+		
+//		then
+		assertThat(result.getId()).isEqualTo(key);
+	}
+	
+	@Test
+	@Transactional
+	void clearStore() {
+		repository.clearStore();
+		assertThat(repository.findAll().size()).isEqualTo(0);
+	}
+
+}
